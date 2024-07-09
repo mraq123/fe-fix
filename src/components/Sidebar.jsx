377d599d38b388f13f0a28b8930d56c5e858a0c8
Muhammad Rafi Al Qodri <72026404+mraq123@users.noreply.github.com>
2024-07-09 13:24:23 +0700

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -38,9 +38,16 @@ const Sidebar = () => {
   }, []);
 
   const Logout = async () => {
-    sessionStorage.removeItem("id");
-    alert("Berhasil Logout");
-    navigate("/");
+    try {
+      const response = await axios.post("https://be-node.vercel.app/logout");
+      if (response.status === 200) {
+        sessionStorage.removeItem("id");
+        alert("Berhasil Logout");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("Failed to logout:", error);
+    }
   };
   return (
     <div>
